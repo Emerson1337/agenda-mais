bd0b5f6abfe86e2c63aa3d666e2c7e552bcf03d3
Emerson <emersonlucenaoficial@gmail.com>
2024-11-27 14:32:09 -0300

    add error utils function to verify axios response and set errors within react hook form

---
+++ frontend/src/shared/utils/errorUtils.ts
@@ -0,0 +1,33 @@
+import { AxiosHeaders, AxiosResponse } from "axios";
+
+import { FieldValues, Path, UseFormSetError } from "react-hook-form";
+
+type ErrorResponse<T> = {
+  body: {
+    errors: {
+      name: Path<T>;
+      stack: string;
+      message: string;
+    }[];
+  };
+};
+
+const applyErrorsToForm = <T extends FieldValues>(
+  setError: UseFormSetError<T>,
+  errorResponse: ErrorResponse<T>
+) => {
+  if (!errorResponse?.body?.errors) return;
+
+  errorResponse.body.errors.forEach((error) =>
+    setError(error.name, {
+      type: error.stack,
+      message: error.message,
+    })
+  );
+};
+
+export default applyErrorsToForm;
+
+export const isAxiosResponse = (error: unknown): error is AxiosResponse => {
+  return !!((error as AxiosResponse)?.headers instanceof AxiosHeaders);
+};
