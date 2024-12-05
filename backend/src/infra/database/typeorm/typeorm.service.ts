563fb4bf3d0432d25ad7f4e90f34b7d3e5238529
Emerson <emersonlucenaoficial@gmail.com>
2024-12-04 21:14:24 -0300

    remove db credentials non used

--- backend/src/infra/database/typeorm/typeorm.service.ts
+++ backend/src/infra/database/typeorm/typeorm.service.ts
@@ -16,10 +16,6 @@ export class TypeormService extends DataSource implements OnModuleInit {
     super({
       type: 'mongodb',
       url: process.env.DB_URL,
-      // host: process.env.DB_HOST,
-      // port: Number(process.env.DB_PORT),
-      // username: process.env.DB_USERNAME,
-      // password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       logging: true,
       entities: [
