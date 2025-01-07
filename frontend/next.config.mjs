7859f51a23ae44047631241f205a70aa065f6ff9
Emerson <emersonlucenaoficial@gmail.com>
2025-01-07 14:12:33 -0300

    change variables

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
   },
   async rewrites() {
     const API_BASE_URL =
-      process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000"; // Fallback if .env is not set
+      process.env.REACT_APP_API_BASE_URL || "http://localhost:3000"; // Fallback if .env is not set
     return [
       {
         // Proxy API requests from `/api` to the configured API base URL
