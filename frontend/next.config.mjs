06144a3e56323b59526555370d71f8642eb1554c
Emerson <emersonlucenaoficial@gmail.com>
2025-01-07 16:33:10 -0300

    change fallback url

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
   },
   async rewrites() {
     const API_BASE_URL =
-      process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000"; // Fallback if .env is not set
+      process.env.NEXT_PUBLIC_API_BASE_URL || "http://fallback.nextconfig"; // Fallback if .env is not set
     return [
       {
         // Proxy API requests from `/api` to the configured API base URL
