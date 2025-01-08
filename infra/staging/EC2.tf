2c12b6d615c53c7adb9de293021c14cd18acb042
Emerson <emersonlucenaoficial@gmail.com>
2025-01-07 22:59:24 -0300

    final ec2 adjustments

--- infra/staging/EC2.tf
+++ infra/staging/EC2.tf
@@ -45,3 +45,48 @@ resource "aws_instance" "agendazap-web-server" {
   user_data = file("./user_data.sh")
   vpc_security_group_ids = [aws_security_group.agendazap-security-group.id]
 }
+
+resource "aws_lb" "agendazap_lb" {
+  name               = "agendazap-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.agendazap-security-group.id]
+  subnets            = ["subnet-0e5e9017a50b6e7b0", "subnet-011baa251d338c6ff", "subnet-0ce7f58bded769b33"]
+}
+
+resource "aws_lb_target_group" "agendazap_target_group" {
+  name     = "agendazap-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "vpc-0a315c5eae678cd2d"
+}
+
+resource "aws_lb_target_group_attachment" "agendazap_attachment" {
+  target_group_arn = aws_lb_target_group.agendazap_target_group.arn
+  target_id        = aws_instance.agendazap-web-server.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "https_listener" {
+  load_balancer_arn = aws_lb.agendazap_lb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = "arn:aws:acm:sa-east-1:294562550042:certificate/9986e96c-29d2-41f5-b860-3a062d7e098b"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.agendazap_target_group.arn
+  }
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.agendazap_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.agendazap_target_group.arn
+  }
+}
