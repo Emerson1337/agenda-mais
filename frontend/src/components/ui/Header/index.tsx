8995fc44e737f3a462df3d62f9d0b00d871688e8
Emerson <emersonlucenaoficial@gmail.com>
2025-01-18 17:05:29 -0300

    improve header

--- frontend/src/components/ui/Header/index.tsx
+++ frontend/src/components/ui/Header/index.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 
 import menuData from "./menuData";
 import { useTheme } from "next-themes";
+import clsx from "clsx";
 
 const Header = () => {
   const pathUrl = usePathname();
@@ -77,40 +78,41 @@ const Header = () => {
                   className="absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
                 >
                   <span
-                    className={`relative my-1.5 block h-0.5 w-[30px] transition-all duration-300 ${
-                      navbarOpen ? " top-[7px] rotate-45" : " "
-                    } ${pathUrl !== "/" && "!bg-dark dark:!bg-black"} ${
-                      pathUrl === "/" && sticky
-                        ? "bg-dark dark:bg-secondary"
-                        : "bg-secondary"
-                    }`}
+                    className={clsx(
+                      "relative my-1.5 block h-0.5 w-[30px] transition-all duration-300",
+                      navbarOpen && "top-[7px] rotate-45",
+                      pathUrl !== "/" && "!bg-dark dark:!bg-black",
+                      pathUrl === "/" &&
+                        (sticky ? "bg-dark dark:bg-secondary" : "bg-secondary"),
+                    )}
                   />
                   <span
-                    className={`relative my-1.5 block h-0.5 w-[30px] transition-all duration-300 ${
-                      navbarOpen ? "opacity-0 " : " "
-                    } ${pathUrl !== "/" && "!bg-dark dark:!bg-black"} ${
-                      pathUrl === "/" && sticky
-                        ? "bg-dark dark:bg-secondary"
-                        : "bg-secondary"
-                    }`}
+                    className={clsx(
+                      "relative my-1.5 block h-0.5 w-[30px] transition-all duration-300",
+                      navbarOpen && "opacity-0",
+                      pathUrl !== "/" && "!bg-dark dark:!bg-black",
+                      pathUrl === "/" &&
+                        (sticky ? "bg-dark dark:bg-secondary" : "bg-secondary"),
+                    )}
                   />
                   <span
-                    className={`relative my-1.5 block h-0.5 w-[30px] transition-all duration-300 ${
-                      navbarOpen ? " top-[-8px] -rotate-45" : " "
-                    } ${pathUrl !== "/" && "!bg-dark dark:!bg-black"} ${
-                      pathUrl === "/" && sticky
-                        ? "bg-dark dark:bg-secondary"
-                        : "bg-secondary"
-                    }`}
+                    className={clsx(
+                      "relative my-1.5 block h-0.5 w-[30px] transition-all duration-300",
+                      navbarOpen && "top-[-8px] -rotate-45",
+                      pathUrl !== "/" && "!bg-dark dark:!bg-black",
+                      pathUrl === "/" &&
+                        (sticky ? "bg-dark dark:bg-secondary" : "bg-secondary"),
+                    )}
                   />
                 </button>
                 <nav
                   id="navbarCollapse"
-                  className={`navbar absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-black px-6 py-4 duration-300 dark:border-body-color/20 dark:bg-dark-2 lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 lg:dark:bg-transparent ${
+                  className={clsx(
+                    "navbar absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-black px-6 py-4 duration-300 dark:border-body-color/20 dark:bg-dark-2 lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 lg:dark:bg-transparent",
                     navbarOpen
                       ? "visibility top-full opacity-100"
-                      : "invisible top-[120%] opacity-0"
-                  }`}
+                      : "invisible top-[120%] opacity-0",
+                  )}
                 >
                   <ul className="block lg:ml-8 lg:flex lg:gap-x-8 xl:ml-14 xl:gap-x-12">
                     <li className="block sm:hidden group relative my-2">
@@ -130,11 +132,24 @@ const Header = () => {
                               onClick={navbarToggleHandler}
                               scroll={false}
                               href={menuItem.path}
-                              className={`ud-menu-scroll flex py-2 text-base  lg:inline-flex lg:px-0 lg:py-6 ${pathUrl === menuItem?.path && !sticky && "!text-primary"} ${pathUrl === menuItem?.path && sticky && "!text-primary"} ${
+                              className={clsx(
+                                "ud-menu-scroll flex py-2 text-base lg:inline-flex lg:px-0 lg:py-6",
+                                pathUrl === menuItem?.path &&
+                                  !sticky &&
+                                  "!text-primary",
+                                pathUrl === menuItem?.path &&
+                                  sticky &&
+                                  "!text-primary",
                                 sticky
-                                  ? "lg:text-secondary-foreground text-white"
-                                  : "lg:text-secondary-foreground text-white "
-                              }`}
+                                  ? [
+                                      "lg:text-secondary-foreground",
+                                      "text-white",
+                                    ]
+                                  : [
+                                      "lg:text-secondary-foreground",
+                                      "text-white",
+                                    ],
+                              )}
                             >
                               {menuItem.title}
                             </Link>
@@ -145,11 +160,22 @@ const Header = () => {
                               className={`ud-menu-scroll flex py-2 text-base lg:inline-flex lg:px-0 lg:py-6 text-white`}
                             >
                               <span
-                                className={`${pathUrl === menuItem?.path && !sticky && "lg:!text-black text-primary"} ${pathUrl === menuItem?.path && sticky && "!text-primary"} ${
+                                className={clsx(
+                                  pathUrl === menuItem?.path &&
+                                    !sticky && [
+                                      "lg:!text-black",
+                                      "text-primary",
+                                    ],
+                                  pathUrl === menuItem?.path &&
+                                    sticky &&
+                                    "!text-primary",
                                   sticky
-                                    ? "lg:text-secondary-foreground text-white"
-                                    : "lg:text-secondary"
-                                }`}
+                                    ? [
+                                        "lg:text-secondary-foreground",
+                                        "text-white",
+                                      ]
+                                    : "lg:text-secondary",
+                                )}
                               >
                                 {menuItem.title}
                               </span>
@@ -253,32 +279,6 @@ const Header = () => {
                 >
                   Acessar Plataforma
                 </Link>
-                {/* theme toggler
-                <button
-                  aria-label="theme toggler"
-                  onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-                  className={`flex h-8 w-8 items-center justify-center text-body-color duration-300 ${sticky ? "dark:text-secondary" : "dark:text-secondary-foreground"}`}
-                >
-                  <span>
-                    <svg
-                      viewBox="0 0 16 16"
-                      className="hidden h-[22px] w-[22px] fill-current dark:block"
-                    >
-                      <path d="M4.50663 3.2267L3.30663 2.03337L2.36663 2.97337L3.55996 4.1667L4.50663 3.2267ZM2.66663 7.00003H0.666626V8.33337H2.66663V7.00003ZM8.66663 0.366699H7.33329V2.33337H8.66663V0.366699V0.366699ZM13.6333 2.97337L12.6933 2.03337L11.5 3.2267L12.44 4.1667L13.6333 2.97337ZM11.4933 12.1067L12.6866 13.3067L13.6266 12.3667L12.4266 11.1734L11.4933 12.1067ZM13.3333 7.00003V8.33337H15.3333V7.00003H13.3333ZM7.99996 3.6667C5.79329 3.6667 3.99996 5.46003 3.99996 7.6667C3.99996 9.87337 5.79329 11.6667 7.99996 11.6667C10.2066 11.6667 12 9.87337 12 7.6667C12 5.46003 10.2066 3.6667 7.99996 3.6667ZM7.33329 14.9667H8.66663V13H7.33329V14.9667ZM2.36663 12.36L3.30663 13.3L4.49996 12.1L3.55996 11.16L2.36663 12.36Z" />
-                    </svg>
-
-                    <svg
-                      viewBox="0 0 23 23"
-                      className={`h-[30px] w-[30px] fill-current text-primary dark:hidden ${
-                        !sticky && pathUrl === "/" && "text-secondary"
-                      }`}
-                    >
-                      <g clipPath="url(#clip0_40_125)">
-                        <path d="M16.6111 15.855C17.591 15.1394 18.3151 14.1979 18.7723 13.1623C16.4824 13.4065 14.1342 12.4631 12.6795 10.4711C11.2248 8.47905 11.0409 5.95516 11.9705 3.84818C10.8449 3.9685 9.72768 4.37162 8.74781 5.08719C5.7759 7.25747 5.12529 11.4308 7.29558 14.4028C9.46586 17.3747 13.6392 18.0253 16.6111 15.855Z" />
-                      </g>
-                    </svg>
-                  </span>
-                </button> */}
               </div>
             </div>
           </div>
