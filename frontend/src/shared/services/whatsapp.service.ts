0419da5a0f4947583440b9c913913c0e579ebd74
Emerson <emersonlucenaoficial@gmail.com>
2025-01-22 11:21:47 -0300

    replace business number

--- frontend/src/shared/services/whatsapp.service.ts
+++ frontend/src/shared/services/whatsapp.service.ts
@@ -67,14 +67,14 @@ export class WhatsappService {
   static contactForDeal(customMessage?: string): string {
     const message =
       customMessage ?? `👋 *Olá! Gostaria de entender mais sobre o AgendaMais.`;
-    const phone = "5585986160705";
+    const phone = "5585989546398";
 
     return `https://api.whatsapp.com/send/?phone=${phone}&text=${message}`;
   }
 
   static contactAboutPlan(plan: string): string {
     const message = `👋 *Olá! Gostaria de entender mais sobre o plano ${plan} do AgendaMais.`;
-    const phone = "5585986160705";
+    const phone = "5585989546398";
 
     return `https://api.whatsapp.com/send/?phone=${phone}&text=${message}`;
   }
