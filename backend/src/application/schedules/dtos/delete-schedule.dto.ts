a27fd614627024b3c3f031c833740e350c46bcee
Emerson <emersonlucenaoficial@gmail.com>
2025-02-04 19:09:49 -0300

    remove mongo id validation

--- backend/src/application/schedules/dtos/delete-schedule.dto.ts
+++ backend/src/application/schedules/dtos/delete-schedule.dto.ts
@@ -1,13 +1,9 @@
-import { IsArrayOfMongoIds } from '@/application/shared/decorators/array-valid-mongoid.decorator';
 import { Type } from 'class-transformer';
 import { IsArray, IsNotEmpty } from 'class-validator';
 
 export class DeleteScheduleDto {
   @IsArray({ message: 'O campo schedulesIds deve ser um array.' })
   @Type(() => String)
-  @IsArrayOfMongoIds({
-    message: 'Cada ID em schedulesIds deve ser um ID MongoDB válido.',
-  })
   schedulesIds: string[];
 
   @IsNotEmpty({ message: 'O campo userId não pode estar vazio.' })
