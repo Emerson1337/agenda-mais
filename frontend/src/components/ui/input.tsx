1b4801da76f04ea79ff7a5d4f17751f6c3b18e84
Emerson <emersonlucenaoficial@gmail.com>
2025-02-10 23:05:58 -0300

    add password icon

--- frontend/src/components/ui/input.tsx
+++ frontend/src/components/ui/input.tsx
@@ -5,18 +5,42 @@ import { cn } from "@/lib/utils";
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
+import { useState } from "react";
+import { EyeOpenIcon, EyeClosedIcon } from "@radix-ui/react-icons";
+
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const toggleShowPassword = () => {
+      setShowPassword((prev) => !prev);
+    };
+
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className,
+      <div className="relative flex items-center">
+        <input
+          type={type === "password" && showPassword ? "text" : type}
+          className={cn(
+            "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+        {type === "password" && (
+          <button
+            type="button"
+            onClick={toggleShowPassword}
+            className="absolute right-2"
+          >
+            {showPassword ? (
+              <EyeClosedIcon className="h-5 w-5 text-gray-500" />
+            ) : (
+              <EyeOpenIcon className="h-5 w-5 text-gray-500" />
+            )}
+          </button>
         )}
-        ref={ref}
-        {...props}
-      />
+      </div>
     );
   },
 );
