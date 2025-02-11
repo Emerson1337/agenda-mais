8084a1a3a695232e4b2e62867bcef7ab7b771e47
Emerson <emersonlucenaoficial@gmail.com>
2025-02-10 23:03:52 -0300

    fix strong password message

--- backend/src/application/auth/dtos/reset-password-dto.ts
+++ backend/src/application/auth/dtos/reset-password-dto.ts
@@ -4,7 +4,19 @@ import { IsNotEmpty, IsString, IsStrongPassword } from 'class-validator';
 export class ResetDto {
   @IsString({ message: 'O campo senha deve ser uma string.' })
   @IsNotEmpty({ message: 'O campo senha não pode estar vazio.' })
-  @IsStrongPassword({}, { message: 'A senha deve ser forte.' })
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minLowercase: 1,
+      minNumbers: 1,
+      minUppercase: 1,
+      minSymbols: 1,
+    },
+    {
+      message:
+        'A senha deve ter no mínimo 8 caracteres, e pelo menos 1 letra maiúscula, 1 letra minúscula, 1 número e 1 caractere especial.',
+    },
+  )
   password: string;
 
   @IsString({ message: 'O campo confirmar senha deve ser uma string.' })
